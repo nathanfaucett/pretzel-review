844558c6e08c8f220cb024c744cab55f2626390a
Nathan Faucett <nathanfaucett@gmail.com>
2024-06-21 21:03:16 -0400

    add post Meltons App and Tap

---
+++ src/lib/posts/1719018191808-meltons-app-and-tap.ts
@@ -0,0 +1,20 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1719018191808-meltons-app-and-tap-0.webp';
+
+const post: Post = {
+  title: 'Meltons App and Tap',
+  uri: 'meltons-app-and-tap',
+  description: `Came with a super below average nacho cheese dip and a basic stone ground mustard`,
+  review: {
+    flavor: 7.2,
+    salt: 5.5,
+    shape: 6.2,
+    texture: 6.6
+  },
+  address: '2500 N Decatur Rd Decatur, GA 30033',
+  images: [image0],
+  updatedTs: 1719018191808,
+  createdTs: 1719018191808
+};
+
+export default post;
