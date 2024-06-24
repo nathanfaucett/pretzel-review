54c7bca993f0fea4904e303ce81f7839f734ee78
Nathan Faucett <nathanfaucett@gmail.com>
2024-06-24 19:11:55 -0400

    add post The Springs Cinema & Taphouse

---
+++ src/lib/posts/1719270711525-the-springs-cinema--taphouse.ts
@@ -0,0 +1,20 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1719270711525-the-springs-cinema--taphouse-0.webp';
+
+const post: Post = {
+  title: 'The Springs Cinema & Taphouse',
+  uri: 'the-springs-cinema--taphouse',
+  description: `Comes with meats, cheeses, pickles, and jalapeños. Also stone mustard and some mid beer cheese sauce`,
+  review: {
+    flavor: 8.3,
+    salt: 1,
+    shape: 5.6,
+    texture: 6.1
+  },
+  address: '5920 Roswell Rd Unit C-103, Sandy Springs, GA 30328',
+  images: [image0],
+  updatedTs: 1719270711525,
+  createdTs: 1719270711525
+};
+
+export default post;
