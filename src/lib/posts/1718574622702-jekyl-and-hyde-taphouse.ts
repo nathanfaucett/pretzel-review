8881c97d0cdbb8eae50daa1f6de69e48c998bb46
Nathan Faucett <nathanfaucett@gmail.com>
2024-06-24 19:27:28 -0400

    edit post Jekyl and Hyde Taphouse

--- src/lib/posts/1718574622702-jekyl-and-hyde-taphouse.ts
+++ src/lib/posts/1718574622702-jekyl-and-hyde-taphouse.ts
@@ -2,19 +2,19 @@ import type { Post } from '.';
 import image0 from '$lib/assets/1718574622702-jekyl-and-hyde-taphouse-0.webp';
 
 const post: Post = {
-	title: 'Jekyl and Hyde Taphouse',
-	uri: 'jekyl-and-hyde-taphouse',
-	description: `Came with limerick beer cheese and honey mustard.`,
-	review: {
-		flavor: 5.8,
-		salt: 7,
-		shape: 9,
-		texture: 6.2
-	},
-	address: '316 N Trade St. Matthews, NC 28105',
-	images: [image0],
-	updatedTs: 1718575019218,
-	createdTs: 1718574622702
+  title: 'Jekyl and Hyde Taphouse',
+  uri: 'jekyl-and-hyde-taphouse',
+  description: `Came with limerick beer cheese and honey mustard.`,
+  review: {
+    flavor: 5.8,
+    salt: 7,
+    shape: 9,
+    texture: 6.2
+  },
+  address: '316 N Trade St. Matthews, NC 28105',
+  images: [image0],
+  updatedTs: 1719271645166,
+  createdTs: 1718574622702
 };
 
 export default post;
