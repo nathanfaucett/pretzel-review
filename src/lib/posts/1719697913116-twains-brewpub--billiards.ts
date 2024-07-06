7145d4f7043673ba2d8e626f4c24fbe952a0784b
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-06 08:10:32 -0400

    edit post Twain's Brewpub & Billiards

--- src/lib/posts/1719697913116-twains-brewpub--billiards.ts
+++ src/lib/posts/1719697913116-twains-brewpub--billiards.ts
@@ -2,20 +2,19 @@ import type { Post } from '.';
 import image0 from '$lib/assets/1719697913116-twains-brewpub--billiards-0.webp';
 
 const post: Post = {
-	title: "Twain's Brewpub & Billiards",
-	uri: 'twains-brewpub--billiards',
-	description: `Came with a wetter beer cheese that dipped surprisingly well, and a fantastic spicy mustard with a hint of Wasabi or horse radish.`,
-	review: {
-		flavor: 5.8,
-		salt: 5.4,
-		shape: 3.7,
-		texture: 3.1,
-		sides: 6.3
-	},
-	address: '211 E Trinity Pl, Decatur, GA 30030',
-	images: [image0],
-	updatedTs: 1719697913116,
-	createdTs: 1719697913116
+  title: 'Twain\'s Brewpub & Billiards',
+  uri: 'twains-brewpub--billiards',
+  description: `Came with a wetter beer cheese that dipped surprisingly well, and a fantastic spicy mustard with a hint of Wasabi or horse radish.`,
+  review: {
+    flavor: 5.8,
+    salt: 5.4,
+    shape: 4.7,
+    texture: 3.1
+  },
+  address: '211 E Trinity Pl, Decatur, GA 30030',
+  images: [image0],
+  updatedTs: 1720267829975,
+  createdTs: 1719697913116
 };
 
 export default post;
