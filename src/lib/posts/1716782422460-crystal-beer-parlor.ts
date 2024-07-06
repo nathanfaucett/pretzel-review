27528c7be90e81e6b1f197639ef9a693254c3053
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-06 08:09:33 -0400

    edit post Crystal Beer Parlor

--- src/lib/posts/1716782422460-crystal-beer-parlor.ts
+++ src/lib/posts/1716782422460-crystal-beer-parlor.ts
@@ -2,20 +2,19 @@ import type { Post } from '.';
 import image0 from '$lib/assets/1716782422460-crystal-beer-parlor-0.webp';
 
 const post: Post = {
-	title: 'Crystal Beer Parlor',
-	uri: 'crystal-beer-parlor',
-	description: `Came with Gouda cheese dipping sauce and it was quite good.`,
-	review: {
-		flavor: 7.1,
-		salt: 1.5,
-		shape: 5.1,
-		texture: 4.8,
-		sides: 7.4
-	},
-	address: '301 W Jones St, Savannah, GA 31401',
-	images: [image0],
-	updatedTs: 1718575184104,
-	createdTs: 1716782422460
+  title: 'Crystal Beer Parlor',
+  uri: 'crystal-beer-parlor',
+  description: `Came with Gouda cheese dipping sauce and it was quite good.`,
+  review: {
+    flavor: 6.1,
+    salt: 1.5,
+    shape: 5.1,
+    texture: 4.8
+  },
+  address: '301 W Jones St, Savannah, GA 31401',
+  images: [image0],
+  updatedTs: 1720267770710,
+  createdTs: 1716782422460
 };
 
 export default post;
