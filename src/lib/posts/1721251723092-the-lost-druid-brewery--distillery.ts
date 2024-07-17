ea62e5fffad5dd08a45657f3731eec913fa5abe4
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-17 17:28:44 -0400

    add post The Lost Druid Brewery & Distillery

---
+++ src/lib/posts/1721251723092-the-lost-druid-brewery--distillery.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1721251723092-the-lost-druid-brewery--distillery-0.webp';
+
+const post: Post = {
+  title: 'The Lost Druid Brewery & Distillery',
+  uri: 'the-lost-druid-brewery--distillery',
+  description: `The salt is bacon. just Came with a very liquidy homemade style whole mustard seed horseradish dip, and some beer cheese.`,
+  review: {
+    flavor: 2.3,
+    salt: 4.2,
+    shape: 1,
+    texture: 1.2,
+    sides: undefined
+  },
+  address: '2866 Washington St, Avondale Estates, GA 30002',
+  images: [image0],
+  updatedTs: 1721251723092,
+  createdTs: 1721251723092
+};
+
+export default post;
