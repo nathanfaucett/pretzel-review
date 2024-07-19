76c3def93a637c3b77b26f2a2f03331e6476e809
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-19 15:40:01 -0400

    add post Mellow Mushroom Decatur

---
+++ src/lib/posts/1721417998627-mellow-mushroom-decatur.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1721417998627-mellow-mushroom-decatur-0.webp';
+
+const post: Post = {
+  title: 'Mellow Mushroom Decatur',
+  uri: 'mellow-mushroom-decatur',
+  description: `Its pretty much pizza crust but theirs is great, comes with marinara sauce.`,
+  review: {
+    flavor: 8.3,
+    salt: 5.2,
+    shape: 3.4,
+    texture: 6.3,
+    sides: 3.3
+  },
+  address: '340 W Ponce de Leon Ave, Decatur, GA 30030',
+  images: [image0],
+  updatedTs: 1721417998627,
+  createdTs: 1721417998627
+};
+
+export default post;
