c67f554efe922fed4629e5de7b63fbe634f27209
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-27 14:06:06 -0400

    add post Cypress Street Pint & Plate

---
+++ src/lib/posts/1722103563963-cypress-street-pint--plate.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1722103563963-cypress-street-pint--plate-0.webp';
+
+const post: Post = {
+  title: 'Cypress Street Pint & Plate',
+  uri: 'cypress-street-pint--plate',
+  description: `Would come back for here for the pretzel alone! The sides were a delicious blend of something classic and something new, you'll get a delectable pub cheese and a spicy BBQ mustard that's a real standout!`,
+  review: {
+    flavor: 8.7,
+    salt: 6.9,
+    shape: 9.6,
+    texture: 8.8,
+    sides: 9.8
+  },
+  address: '817 W Peachtree St NW, Atlanta, GA 30308',
+  images: [image0],
+  updatedTs: 1722103563963,
+  createdTs: 1722103563963
+};
+
+export default post;
