7a7741b748a435bd644e3faa29bd14cd05982d24
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-27 12:47:43 -0400

    add post Thinking Man Tavern

---
+++ src/lib/posts/1722098861162-thinking-man-tavern.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1722098861162-thinking-man-tavern-0.webp';
+
+const post: Post = {
+  title: 'Thinking Man Tavern',
+  uri: 'thinking-man-tavern',
+  description: `One reviewer said it reminded her of corndogs, the cheese dip was fantastic and the mustard is near perfect for pretzels.`,
+  review: {
+    flavor: 4.6,
+    salt: 7.1,
+    shape: 4.4,
+    texture: 3.9,
+    sides: 9.6
+  },
+  address: '537 W Howard Ave, Decatur, GA 30030',
+  images: [image0],
+  updatedTs: 1722098861162,
+  createdTs: 1722098861162
+};
+
+export default post;
