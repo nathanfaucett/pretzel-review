329c7086f9d79dbd896323a9f65a72cb55c0e2c8
Nathan Faucett <nathanfaucett@gmail.com>
2024-08-07 18:14:12 -0400

    fix prerender

--- src/routes/(unauthed)/+page.ts
+++ src/routes/(unauthed)/+page.ts
@@ -1,6 +1,9 @@
 import { posts } from '$lib/posts';
 import type { PageLoad } from './$types';
 
+export const prerender = true;
+export const ssr = true;
+
 export const load: PageLoad = async (event) => {
 	await event.parent();
 	return {
