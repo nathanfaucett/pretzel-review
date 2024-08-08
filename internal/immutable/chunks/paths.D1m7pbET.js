5dad808e0fefe867c11745693daf35bf825144fe
nathanfaucett <nathanfaucett@users.noreply.github.com>
2024-08-08 18:21:03 +0000

    deploy: d33b7ecebf4601b31951b85a2081ed6220326f71

--- internal/immutable/chunks/paths.D1m7pbET.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_16xll77)==null?void 0:s.base)??"/pretzels";var e;const t=((e=globalThis.__sveltekit_16xll77)==null?void 0:e.assets)??l;export{t as a,l as b};
