25145e38f3501777a23d3e9f87a658411fe727e1
Nathan Faucett <nathanfaucett@gmail.com>
2024-08-08 14:20:37 -0400

    add post Sam Adams Atlanta Brew House

---
+++ src/lib/posts/1723141235240-sam-adams-atlanta-brew-house.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1723141235240-sam-adams-atlanta-brew-house-0.webp';
+
+const post: Post = {
+  title: 'Sam Adams Atlanta Brew House',
+  uri: 'sam-adams-atlanta-brew-house',
+  description: `Looking for a classic but delicious pretzel? This is it! Delicious flavor moist center and decent sides. The mustard is your classic whole seed yellow mustard and a nice pub cheese topped with paprika for "fancy" flare.`,
+  review: {
+    flavor: 8.5,
+    salt: 6.8,
+    shape: 6.9,
+    texture: 8.6,
+    sides: 7.1
+  },
+  address: 'Hartsfield-Jackson Atlanta International Airport, Concourse B, Intl Airport, N Terminal Pkwy Suite T120, Atlanta, GA 30320',
+  images: [image0],
+  updatedTs: 1723141235240,
+  createdTs: 1723141235240
+};
+
+export default post;
