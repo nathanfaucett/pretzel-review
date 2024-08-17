73257c48003078b5f896a49a8bee2a61e80d3b19
Nathan Faucett <nathanfaucett@gmail.com>
2024-08-17 19:43:38 -0400

    add post The Porter Beer Bar

---
+++ src/lib/posts/1723938214780-the-porter-beer-bar.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1723938214780-the-porter-beer-bar-0.webp';
+
+const post: Post = {
+  title: 'The Porter Beer Bar',
+  uri: 'the-porter-beer-bar',
+  description: `Don't just a pretzel from its shape. This pretzel is serving the delectable outer shell crunch we all crave with a perfectly balance d middle that will hang on your tastebuds for long after your meal is complete.`,
+  review: {
+    flavor: 9.7,
+    salt: 5.1,
+    shape: 3,
+    texture: 9.9,
+    sides: 6.7
+  },
+  address: '1156 Euclid Ave NE, Atlanta, GA 30307',
+  images: [image0],
+  updatedTs: 1723938214780,
+  createdTs: 1723938214780
+};
+
+export default post;
