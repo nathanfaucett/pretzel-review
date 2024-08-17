5e891c2d9ebe158ac10e5feb747d419d4deee6a8
Nathan Faucett <nathanfaucett@gmail.com>
2024-08-17 19:43:39 -0400

    update src/lib/posts/index.ts

--- src/lib/posts/index.ts
+++ src/lib/posts/index.ts
@@ -1,3 +1,4 @@
+import post1723938214780 from './1723938214780-the-porter-beer-bar';
 import post1723931426927 from './1723931426927-the-albert';
 import post1723141235240 from './1723141235240-sam-adams-atlanta-brew-house';
 import post1717646487699 from './1717646487699-benchwarmers';
@@ -59,3 +60,4 @@ posts[`${post1722103563963.createdTs}-${post1722103563963.uri}`] = post172210356
 posts[`${post1717646487699.createdTs}-${post1717646487699.uri}`] = post1717646487699;
 posts[`${post1723141235240.createdTs}-${post1723141235240.uri}`] = post1723141235240;
 posts[`${post1723931426927.createdTs}-${post1723931426927.uri}`] = post1723931426927;
+posts[`${post1723938214780.createdTs}-${post1723938214780.uri}`] = post1723938214780;
