878e751c5fdf807e35e2d2fdd379531d49038ca8
Nathan Faucett <nathanfaucett@gmail.com>
2024-08-18 06:44:56 -0400

    edit "The Albert"

--- src/lib/posts/1723931426927-the-albert.ts
+++ src/lib/posts/1723931426927-the-albert.ts
@@ -2,20 +2,20 @@ import type { Post } from '.';
 import image0 from '$lib/assets/1723931426927-the-albert-0.webp';
 
 const post: Post = {
-  title: 'The Albert',
-  uri: 'the-albert',
-  description: `"Thats was frozen"  at first bite the post frozen texture overtakes the flavor of this visually promising pretzel. The dipping cheese was on its second reheat from its canned origins.`,
-  review: {
-    flavor: 3.9,
-    salt: 4.9,
-    shape: 5.5,
-    texture: 4.1,
-    sides: 3.6
-  },
-  address: '918 Austin Ave NE, Atlanta, GA 30307',
-  images: [image0],
-  updatedTs: 1723931426927,
-  createdTs: 1723931426927
+	title: 'The Albert',
+	uri: 'the-albert',
+	description: `At first bite the post frozen texture overtakes the flavor of this visually promising pretzel. The dipping cheese was on its second reheat from its canned origins.`,
+	review: {
+		flavor: 3.9,
+		salt: 4.9,
+		shape: 5.5,
+		texture: 4.1,
+		sides: 3.6
+	},
+	address: '918 Austin Ave NE, Atlanta, GA 30307',
+	images: [image0],
+	updatedTs: 1723931426927,
+	createdTs: 1723931426927
 };
 
 export default post;
