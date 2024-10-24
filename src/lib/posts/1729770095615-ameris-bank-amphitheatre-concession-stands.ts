8304c5b54418731b39616edfc2a024fccdc1336b
Nathan Faucett <nathanfaucett@gmail.com>
2024-10-24 07:41:37 -0400

    add post Ameris Bank Amphitheatre: Concession stands

---
+++ src/lib/posts/1729770095615-ameris-bank-amphitheatre-concession-stands.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1729770095615-ameris-bank-amphitheatre-concession-stands-0.webp';
+
+const post: Post = {
+  title: 'Ameris Bank Amphitheatre: Concession stands',
+  uri: 'ameris-bank-amphitheatre-concession-stands',
+  description: `This is as basic as it gets, thawed and hung out in one of those pretzel boxes. Left to dry out and ruin your day.`,
+  review: {
+    flavor: 1.2,
+    salt: 4.5,
+    shape: 7.1,
+    texture: 2.1,
+    sides: 0
+  },
+  address: '2200 Encore Pkwy, Alpharetta, GA 30009',
+  images: [image0],
+  updatedTs: 1729770095615,
+  createdTs: 1729770095615
+};
+
+export default post;
