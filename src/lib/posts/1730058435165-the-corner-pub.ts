33194ca07ec6d0f57ddb79e8a2e32409e4f6481e
Nathan Faucett <nathanfaucett@gmail.com>
2024-10-27 15:47:18 -0400

    add post The Corner Pub

---
+++ src/lib/posts/1730058435165-the-corner-pub.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1730058435165-the-corner-pub-0.webp';
+
+const post: Post = {
+  title: 'The Corner Pub',
+  uri: 'the-corner-pub',
+  description: `Deep fried, very crunchy, very salty, rich in flavor needs no sauces.`,
+  review: {
+    flavor: 7.6,
+    salt: 7.9,
+    shape: 9.4,
+    texture: 9.1,
+    sides: 0
+  },
+  address: '627 E College Ave, Decatur, GA 30030',
+  images: [image0],
+  updatedTs: 1730058435165,
+  createdTs: 1730058435165
+};
+
+export default post;
