e5a0fa8d91473e0e76f79e5ddee37c94740d203c
Nathan Faucett <nathanfaucett@gmail.com>
2024-11-11 19:50:52 -0500

    add post Fadó Irish Pub

---
+++ src/lib/posts/1731372650356-fad-irish-pub.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1731372650356-fad-irish-pub-0.webp';
+
+const post: Post = {
+  title: 'Fadó Irish Pub',
+  uri: 'fad-irish-pub',
+  description: `A well  balanced crunchy crust with a fluffy center. Has one of our favorite whole seed mustard side and a flavorful pub cheese.`,
+  review: {
+    flavor: 7.8,
+    salt: 6.1,
+    shape: 8.9,
+    texture: 8.1,
+    sides: 8.1
+  },
+  address: '933 Peachtree St NE, Atlanta, GA 30309',
+  images: [image0],
+  updatedTs: 1731372650356,
+  createdTs: 1731372650356
+};
+
+export default post;
