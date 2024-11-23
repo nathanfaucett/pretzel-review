c404904c48a154cd9538c3278ca53b9de04da720
Nathan Faucett <nathanfaucett@gmail.com>
2024-11-23 12:45:36 -0500

    update src/lib/posts/index.ts

--- src/lib/posts/index.ts
+++ src/lib/posts/index.ts
@@ -1,5 +1,5 @@
-import post1731372650356 from './1731372650356-fad-irish-pub';
 import post1730058435165 from './1730058435165-the-corner-pub';
+import post1731372650356 from './1731372650356-fad-irish-pub';
 import post1729770095615 from './1729770095615-ameris-bank-amphitheatre-concession-stands';
 import post1722098861162 from './1722098861162-thinking-man-tavern';
 import post1723938214780 from './1723938214780-the-porter-beer-bar';
@@ -65,5 +65,5 @@ posts[`${post1723931426927.createdTs}-${post1723931426927.uri}`] = post172393142
 posts[`${post1723938214780.createdTs}-${post1723938214780.uri}`] = post1723938214780;
 posts[`${post1722098861162.createdTs}-${post1722098861162.uri}`] = post1722098861162;
 posts[`${post1729770095615.createdTs}-${post1729770095615.uri}`] = post1729770095615;
-posts[`${post1730058435165.createdTs}-${post1730058435165.uri}`] = post1730058435165;
 posts[`${post1731372650356.createdTs}-${post1731372650356.uri}`] = post1731372650356;
+posts[`${post1730058435165.createdTs}-${post1730058435165.uri}`] = post1730058435165;
