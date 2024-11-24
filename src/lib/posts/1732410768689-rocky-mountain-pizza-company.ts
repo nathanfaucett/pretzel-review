da74e85a10f0dae11fe15028c75c0daa50c8595b
Nathan Faucett <nathanfaucett@gmail.com>
2024-11-23 20:12:51 -0500

    add post Rocky Mountain Pizza Company

---
+++ src/lib/posts/1732410768689-rocky-mountain-pizza-company.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1732410768689-rocky-mountain-pizza-company-0.webp';
+
+const post: Post = {
+  title: 'Rocky Mountain Pizza Company',
+  uri: 'rocky-mountain-pizza-company',
+  description: `Not just a pizza place their pretzel did not disappoint. Crunchy outer shell and well baked fluffy middle. Classic whole seed mustard for the dip was a welcome complement to this tasty treat.`,
+  review: {
+    flavor: 6.9,
+    salt: 7.1,
+    shape: 6.9,
+    texture: 8.1,
+    sides: 5.6
+  },
+  address: '1005 Hemphill Ave NW, Atlanta, GA 30318',
+  images: [image0],
+  updatedTs: 1732410768689,
+  createdTs: 1732410768689
+};
+
+export default post;
