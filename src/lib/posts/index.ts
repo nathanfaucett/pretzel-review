797d4acf1a38a7b0f7fbccc6cf5891d615815522
Nathan Faucett <nathanfaucett@gmail.com>
2024-12-15 17:22:58 -0500

    update src/lib/posts/index.ts

--- src/lib/posts/index.ts
+++ src/lib/posts/index.ts
@@ -1,3 +1,4 @@
+import post1734301373383 from './1734301373383-osullivans-irish-pub';
 import post1730058435165 from './1730058435165-the-corner-pub';
 import post1729770095615 from './1729770095615-ameris-bank-amphitheatre-concession-stands';
 import post1732410768689 from './1732410768689-rocky-mountain-pizza-company';
@@ -69,3 +70,4 @@ posts[`${post1716782422460.createdTs}-${post1716782422460.uri}`] = post171678242
 posts[`${post1732410768689.createdTs}-${post1732410768689.uri}`] = post1732410768689;
 posts[`${post1729770095615.createdTs}-${post1729770095615.uri}`] = post1729770095615;
 posts[`${post1730058435165.createdTs}-${post1730058435165.uri}`] = post1730058435165;
+posts[`${post1734301373383.createdTs}-${post1734301373383.uri}`] = post1734301373383;
