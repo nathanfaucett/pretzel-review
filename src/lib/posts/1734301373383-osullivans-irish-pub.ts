ba5f4cccc211d01ff3d3a756a2abc662177bcb01
Nathan Faucett <nathanfaucett@gmail.com>
2024-12-15 17:22:56 -0500

    add post O'Sullivans Irish Pub

---
+++ src/lib/posts/1734301373383-osullivans-irish-pub.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1734301373383-osullivans-irish-pub-0.webp';
+
+const post: Post = {
+  title: 'O\'Sullivans Irish Pub',
+  uri: 'osullivans-irish-pub',
+  description: `Great classic bavarian pretzel. Shiny crisp outer shell with moist center and adequate salt. Delicious spicy muster and pub cheese was great consistency for dipping.`,
+  review: {
+    flavor: 8.6,
+    salt: 7.3,
+    shape: 9.1,
+    texture: 8.8,
+    sides: 7.6
+  },
+  address: '111 Sycamore St, Decatur, GA 30030',
+  images: [image0],
+  updatedTs: 1734301373383,
+  createdTs: 1734301373383
+};
+
+export default post;
