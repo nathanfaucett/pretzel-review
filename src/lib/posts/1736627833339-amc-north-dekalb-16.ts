2619ed9996de4db55cd0442caf20fcf55c2eb2e3
Nathan Faucett <nathanfaucett@gmail.com>
2025-01-11 15:37:15 -0500

    add post AMC North Dekalb 16

---
+++ src/lib/posts/1736627833339-amc-north-dekalb-16.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1736627833339-amc-north-dekalb-16-0.webp';
+
+const post: Post = {
+  title: 'AMC North Dekalb 16',
+  uri: 'amc-north-dekalb-16',
+  description: `Do you like store brought white bread toasted in a toaster oven covered in fake butter served with a side of high-school concession stand nacho cheese in a pizza box from the 80's? no? neither do we. Please do not order the "Bavarian Legend". It's both a insult to the lovely region of Bavaria, and a wild misuse of the word legend. This is the first pretzel we have ever not finished.`,
+  review: {
+    flavor: 0,
+    salt: 4,
+    shape: 2.3,
+    texture: 0,
+    sides: 0
+  },
+  address: '2042 Lawrenceville Hwy Suite F-22, Decatur, GA 30033',
+  images: [image0],
+  updatedTs: 1736627833339,
+  createdTs: 1736627833339
+};
+
+export default post;
