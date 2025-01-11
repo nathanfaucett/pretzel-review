e90f76de0983e1f8f0592d00651a6e9e16ebf057
Nathan Faucett <nathanfaucett@gmail.com>
2025-01-11 17:26:01 -0500

    add post Brewhouse

---
+++ src/lib/posts/1736634358475-brewhouse.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1736634358475-brewhouse-0.webp';
+
+const post: Post = {
+  title: 'Brewhouse',
+  uri: 'brewhouse',
+  description: `This is a classically shaped American style pretzel. The deep fry gives it a lovely flavor and texture. Do to its size it is more dense and chewy than the larger classic bavarian style but overall is a worth while endeavor. We did not partake in the add on queso but we not disappointed by their delicious house made mustard, a unique flavor experience and a must try.`,
+  review: {
+    flavor: 8.1,
+    salt: 8,
+    shape: 5.5,
+    texture: 7,
+    sides: 8.5
+  },
+  address: '401 Moreland Ave NE, Atlanta, GA 30307',
+  images: [image0],
+  updatedTs: 1736634358475,
+  createdTs: 1736634358475
+};
+
+export default post;
