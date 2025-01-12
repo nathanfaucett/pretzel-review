60770f786db0218ca1c34425f514db37fe7ce66b
nathanfaucett <nathanfaucett@users.noreply.github.com>
2025-01-12 22:28:33 +0000

    deploy: de3b14b78ce4ba97b017346483ed0372af441d6b

---
+++ internal/immutable/chunks/paths.Co8L6mFK.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_rulhsl)==null?void 0:s.base)??"/pretzel-review";var e;const t=((e=globalThis.__sveltekit_rulhsl)==null?void 0:e.assets)??l;export{t as a,l as b};
