5c7ed1fb0dac42236713dc8a6a866e64218c97fd
Nathan Faucett <nathanfaucett@gmail.com>
2025-01-18 16:00:27 -0500

    add post Halfway Crooks Beer

---
+++ src/lib/posts/1737234023124-halfway-crooks-beer.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1737234023124-halfway-crooks-beer-0.webp';
+
+const post: Post = {
+  title: 'Halfway Crooks Beer',
+  uri: 'halfway-crooks-beer',
+  description: `Its probably frozen,  but they warm it up very well and the honey mustard is amazing and worth it alone.`,
+  review: {
+    flavor: 4.9,
+    salt: 5.8,
+    shape: 4.9,
+    texture: 4.8,
+    sides: 9
+  },
+  address: '60 Georgia Ave SE, Atlanta, GA 30312',
+  images: [image0],
+  updatedTs: 1737234023124,
+  createdTs: 1737234023124
+};
+
+export default post;
