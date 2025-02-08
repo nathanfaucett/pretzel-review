6cba0ddd19cead0e45055710311d1290b9ebbc80
Nathan Faucett <nathanfaucett@gmail.com>
2025-02-08 14:09:12 -0500

    add post Atkins Park Restaurant & Bar - Virginia Highland

---
+++ src/lib/posts/1739041748588-atkins-park-restaurant--bar---virginia-highland.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1739041748588-atkins-park-restaurant--bar---virginia-highland-0.webp';
+
+const post: Post = {
+  title: 'Atkins Park Restaurant & Bar - Virginia Highland',
+  uri: 'atkins-park-restaurant--bar---virginia-highland',
+  description: `This was one of our worst mistakes in our lives, we went because on Google they sad they had a Bavarian Pretzel but good lord was google wrong, even the waiter was surprised by how horrible the new "pretzels" looked, he took it away and didn't make us pay for this unflavored, uncooked, monstrosity!`,
+  review: {
+    flavor: 0,
+    salt: 1,
+    shape: 0,
+    texture: 0,
+    sides: 5.1
+  },
+  address: '794 North Highland Avenue Northeast, Atlanta, GA 30306',
+  images: [image0],
+  updatedTs: 1739041748588,
+  createdTs: 1739041748588
+};
+
+export default post;
