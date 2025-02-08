71901572f26e7f3080381d312039604485521ecc
Nathan Faucett <nathanfaucett@gmail.com>
2025-02-08 14:12:56 -0500

    add post New Realm Brewing Co.

---
+++ src/lib/posts/1739041974749-new-realm-brewing-co.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1739041974749-new-realm-brewing-co-0.webp';
+
+const post: Post = {
+  title: 'New Realm Brewing Co.',
+  uri: 'new-realm-brewing-co',
+  description: `This is one of our go-to's, don't let the off color beer cheese distract you, its good, not great. The pretzel itself what you'd want from a Jumbo Bavarian Pretzel. You won't leave disappointed in this big guy.`,
+  review: {
+    flavor: 7.2,
+    salt: 6.2,
+    shape: 9.1,
+    texture: 8.9,
+    sides: 6
+  },
+  address: '550 Somerset Terrace NE #101, Atlanta, GA 30306',
+  images: [image0],
+  updatedTs: 1739041974749,
+  createdTs: 1739041974749
+};
+
+export default post;
