cdb1bdb3fe9de4e24805362baeaec2d956ccde1c
Nathan Faucett <nathanfaucett@gmail.com>
2025-02-10 09:01:18 -0500

    edit post Brick Store Pub

--- src/lib/posts/1721251536575-brick-store-pub.ts
+++ src/lib/posts/1721251536575-brick-store-pub.ts
@@ -2,20 +2,20 @@ import type { Post } from '.';
 import image0 from '$lib/assets/1721251536575-brick-store-pub-0.webp';
 
 const post: Post = {
-	title: 'Brick Store Pub',
-	uri: 'brick-store-pub',
-	description: `Came with a solid stone ground mustard.`,
-	review: {
-		flavor: 5.4,
-		salt: 7.2,
-		shape: 4.2,
-		texture: 5.9,
-		sides: 4.1
-	},
-	address: '125 E Court Square, Decatur, GA 30030',
-	images: [image0],
-	updatedTs: 1721251536575,
-	createdTs: 1721251536575
+  title: 'Brick Store Pub',
+  uri: 'brick-store-pub',
+  description: `Came with a solid stone ground mustard.`,
+  review: {
+    flavor: 4.9,
+    salt: 7.2,
+    shape: 4.2,
+    texture: 5.1,
+    sides: 5
+  },
+  address: '125 E Court Square, Decatur, GA 30030',
+  images: [image0],
+  updatedTs: 1739196070788,
+  createdTs: 1721251536575
 };
 
 export default post;
