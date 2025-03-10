d9edc396abc3490873163ee1789171520d7d9095
Nathan Faucett <nathanfaucett@gmail.com>
2025-03-10 07:54:38 -0400

    add post McCray's Tavern Beltline

---
+++ src/lib/posts/1741607677183-mccrays-tavern-beltline.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1741607677183-mccrays-tavern-beltline-0.webp';
+
+const post: Post = {
+  title: 'McCray\'s Tavern Beltline',
+  uri: 'mccrays-tavern-beltline',
+  description: `Irish Pubs normally have a great beer pretzel and this place does not disappoint, sides are a classic honey mustard and a queso that is a little too wet but gets the job done.`,
+  review: {
+    flavor: 6.1,
+    salt: 6.2,
+    shape: 7.9,
+    texture: 6.9,
+    sides: 5.3
+  },
+  address: '670 DeKalb Ave NE Suite 101, Atlanta, GA 30307',
+  images: [image0],
+  updatedTs: 1741607677183,
+  createdTs: 1741607677183
+};
+
+export default post;
