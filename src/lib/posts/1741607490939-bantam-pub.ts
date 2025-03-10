79453ba020de6423df1df246763289697fe3fa41
Nathan Faucett <nathanfaucett@gmail.com>
2025-03-10 07:51:33 -0400

    add post Bantam Pub

---
+++ src/lib/posts/1741607490939-bantam-pub.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1741607490939-bantam-pub-0.webp';
+
+const post: Post = {
+  title: 'Bantam Pub',
+  uri: 'bantam-pub',
+  description: `Classic Pub pretzel, perfect to have with a Beer. The queso is a great side, has some fresh jalapenos in it that add a little kick.`,
+  review: {
+    flavor: 6.8,
+    salt: 6.9,
+    shape: 8.3,
+    texture: 6.3,
+    sides: 7.1
+  },
+  address: '737 Ralph McGill Blvd NE, Atlanta, GA 30312',
+  images: [image0],
+  updatedTs: 1741607490939,
+  createdTs: 1741607490939
+};
+
+export default post;
