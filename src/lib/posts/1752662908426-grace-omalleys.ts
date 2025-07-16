6b6fd75e69a93d4cf9e3f5f4723784953ec4c0d8
Nathan Faucett <nathanfaucett@gmail.com>
2025-07-16 06:48:30 -0400

    add post Grace OMalleys

---
+++ src/lib/posts/1752662908426-grace-omalleys.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1752662908426-grace-omalleys-0.webp';
+
+const post: Post = {
+  title: 'Grace OMalleys',
+  uri: 'grace-omalleys',
+  description: `Very basic white bread, a bit yeasty but with a nice buttery pretzel crust. Comes with a classic stone ground mustard and a decent pub cheese.`,
+  review: {
+    flavor: 4.8,
+    salt: 5.4,
+    shape: 5.6,
+    texture: 5,
+    sides: 5.6
+  },
+  address: '157 N Trade St, Matthews, NC 28105',
+  images: [image0],
+  updatedTs: 1752662908426,
+  createdTs: 1752662908426
+};
+
+export default post;
