65c1011732502df53a0ccb1b7427e3885a4ba536
Nathan Faucett <nathanfaucett@gmail.com>
2025-07-19 18:30:19 -0400

    edit post Tucker Brewing Company

--- src/lib/posts/1752962562296-tucker-brewing-company.ts
+++ src/lib/posts/1752962562296-tucker-brewing-company.ts
@@ -4,17 +4,17 @@ import image0 from '$lib/assets/1752962562296-tucker-brewing-company-0.webp';
 const post: Post = {
   title: 'Tucker Brewing Company',
   uri: 'tucker-brewing-company',
-  description: `Taste is reminiscent of a funnel cake. Great shape and crunch inside was fluffy but the slightest bit dry not a pretzel easy to enjoy without a beer or dip. Overall a great pretzel and a must try if your in the area.`,
+  description: `Taste is reminiscent of a funnel cake. Great shape and crunch. Inside was fluffy but the slightest bit dry, not a pretzel easy to enjoy without a beer or dip. Overall a great pretzel and a must try if your in the area.`,
   review: {
     flavor: 7,
     salt: 8.3,
     shape: 9.1,
     texture: 6.6,
-    sides: 5.5
+    sides: 5
   },
   address: '2003 S Bibb Dr, Tucker, GA 30084',
   images: [image0],
-  updatedTs: 1752962562296,
+  updatedTs: 1752964212204,
   createdTs: 1752962562296
 };
 
