cdd8cbd196d40516eacd99669665309c9bf0cf26
Nathan Faucett <nathanfaucett@gmail.com>
2026-03-28 19:20:40 -0400

    add post Magic Kingdom Park

---
+++ src/lib/posts/1774740039970-magic-kingdom-park.ts
@@ -0,0 +1,21 @@
+import type { Post } from '.';
+import image0 from '$lib/assets/1774740039970-magic-kingdom-park-0.webp';
+
+const post: Post = {
+  title: 'Magic Kingdom Park',
+  uri: 'magic-kingdom-park',
+  description: `Great look, mid flavor, and high school concession stand nacho cheese. Fun but not worth it for serious pretzel lovers unless your having a Disney breakdown and are desperate for some carbs.`,
+  review: {
+    flavor: 4.7,
+    salt: 7.5,
+    shape: 9.1,
+    texture: 4.8,
+    sides: 2
+  },
+  address: 'Bay Lake, FL 32836',
+  images: [image0],
+  updatedTs: 1774740039970,
+  createdTs: 1774740039970
+};
+
+export default post;
